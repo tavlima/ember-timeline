f9d407b4b89638bb7d0746ef76c6f055b7bb8e45
Thiago Lima <t.augusto@gmail.com>
2016-10-18 00:30:40 -0200

    Hides colliding labels

--- app/components/ember-timeline/component.js
+++ app/components/ember-timeline/component.js
@@ -32,4 +32,43 @@ export default Ember.Component.extend({
       start: this.get('minDate')
     };
   }),
+
+  _updateMilestonesVisibility: function() {
+    let self = this,
+      $this = this.$(),
+      $milestoneTexts = $this.find('.ember-timeline-milestone .ember-timeline-milestone-texts'),
+      $phaseBars = $this.find('.ember-timeline-phase:not(:first-child)');
+
+    let milestoneObjs = $milestoneTexts.map((_, o) => {
+      let rect = o.getBoundingClientRect();
+
+      return {
+        x: [rect.left, rect.right],
+        center: (rect.left + rect.right) / 2,
+        node: o
+      };
+    });
+
+    let phaseObjs = $phaseBars.map((_, o) => {
+      return {
+        x: o.getBoundingClientRect().left,
+        node: o
+      }
+    });
+
+    milestoneObjs.sort((a, b) => {
+      return (a.x[0] - b.x[0]) / Math.abs(a.x[0] - b.x[0]) || 0;
+
+    }).get().reduce((a, b) => {
+      let dontOverlap = (a.x[1] < b.x[0]) || (b.x[1] < a.x[0]),
+          shouldHideA = !dontOverlap && a.center <= b.center,
+          shouldHideB = !dontOverlap && b.center < a.center;
+
+          $(a.node).css('visibility', shouldHideA ? 'hidden' : 'inherit');
+          $(b.node).css('visibility', shouldHideB ? 'hidden' : 'inherit');
+
+          return b;
+    });
+
+  }.on('didRender'),
 });
